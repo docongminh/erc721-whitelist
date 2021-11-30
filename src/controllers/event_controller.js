96c08cf7f878735d90402544fa97a9a033cc8a43
minhdc <minhdc@fabbi.io>
2021-11-30 09:04:22 +0700

    add subcribe events

--- src/controllers/event_controller.js
+++ src/controllers/event_controller.js
@@ -1,9 +1,57 @@
 const BaseInteractive = require('./base');
 
 class SCEvent extends BaseInteractive{
-    constructor(abi, sc_addr){
-        super(abi, sc_addr);
+    constructor(){
+        super();
+        this.subscribedEvents = {};
     }
+    //
+    getSignature(event_name){
+        const events = this.getEvents();
+        for(var event in events){
+            if(events[event].name == event_name){
+                return event;
+            }
+        }
+
+        return null;
+    }
+    //
+    subscribeLogEvent(event_name){
+        const eventSignature = this.getSignature(event_name);
+        if(eventSignature != null){
+            const subscription = this.web3.eth.subscribe('logs',{
+                address: this.contract.options.address,
+                topics: [eventSignature] }, (error, result) => {
+                if (!error) {
+
+                    console.log(`Result '${event_name}' event: `, result)
+                }
+                else{
+                    console.log(`Error '${event_name}' event: `, result)
+                }
+            })
+            this.subscribedEvents[event_name] = subscription;
+        }
+        else{
+            console.log("Can't get Event Signature !")
+        }
+        
+        console.log(`Start subscribed to event '${event_name}' of contract '${this.contract.options.address}' `)
+    }
+
+    //
+    unsubscribeLogEvent(event_name){
+        this.subscribedEvents[event_name].unsubscribe(function(error, success){
+            if(success)
+                console.log(`Successfully unsubscribed '${event_name}'!`);
+        });
+    }
+
 }
+// sce = new SCEvent();
+// sce.subscribeLogEvent("setWhiteListStatus");
 
 module.exports = new SCEvent();
+
+    
